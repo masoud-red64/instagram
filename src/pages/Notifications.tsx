46ab66c3bece334f017451a3aa905df7cd2c2b89
masoud <masoud.red64@gamil.com>
2023-08-08 19:49:17 +0330

    Header of notification page

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import NotificationBox from "../Components/NotificationBox/NotificationBox";
 
 function Notifications() {
-  return <NotificationBox />;
+  return (
+    <div>
+      <div className="h-[45px] flex md:hidden items-center justify-between dark:text-gray-100 px-4 border-b border-[#dbdbdb]">
+        <div>
+          <svg className="w-6 h-6 -rotate-90">
+            <use href="#arrow-top"></use>
+          </svg>
+        </div>
+        <h3 className="font-[600] grow text-center dark:">Notifications</h3>
+        <div className="w-1"></div>
+      </div>
+      <NotificationBox />
+    </div>
+  );
 }
 
 export default Notifications;
