dfb8164b513882cd15c6849c907bb08394303204
masoud <masoud.red64@gamil.com>
2023-08-08 14:54:18 +0330

    add img two

--- src/Components/TwoProfile/TwoProfile.tsx
+++ src/Components/TwoProfile/TwoProfile.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Story from "../Story/Story";
 
-function TwoProfile({ img }: { img: string }) {
+function TwoProfile({ img1, img2 }: { img1: string; img2: string }) {
   return (
     <div className="relative w-11 h-11">
       <div className="absolute bottom-0 right-0 w-9 h-9 border-2 border-white rounded-full z-[1]">
-        <Story img={img} hasStory={false} />
+        <Story img={img1} hasStory={false} />
       </div>
       <div className="absolute top-0 left-0 w-8 h-8">
-        <Story img={img} hasStory={false} />
+        <Story img={img2} hasStory={false} />
       </div>
     </div>
   );
