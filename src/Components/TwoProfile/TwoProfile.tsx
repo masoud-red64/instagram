3d93d41021526109a3569f76da24de28c78d05df
masoud <masoud.red64@gamil.com>
2023-08-09 18:15:21 +0330

    edit

--- src/Components/TwoProfile/TwoProfile.tsx
+++ src/Components/TwoProfile/TwoProfile.tsx
@@ -3,7 +3,7 @@ import Story from "../Story/Story";
 
 function TwoProfile({ img1, img2 }: { img1: string; img2: string }) {
   return (
-    <div className="relative w-12 h-12">
+    <div className="relative w-12 h-12 shrink-0">
       <div className="absolute bottom-0 right-0 w-9 h-9 border border-white dark:border-black rounded-full z-[1]">
         <Story img={img1} hasStory={false} />
       </div>
