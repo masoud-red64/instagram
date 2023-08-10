a6da2053d2ae552ef15018b8ff40e6643a1a7488
masoud <masoud.red64@gamil.com>
2023-08-10 16:39:38 +0330

    Insert CreateNewPost Component And Use Swiper

--- src/Components/CreateNewPost/CreateNewPost.tsx
+++ src/Components/CreateNewPost/CreateNewPost.tsx
@@ -1,20 +1,205 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperType from "swiper";
+
+type imagesType = {
+  id: string;
+  img: string;
+};
 
 function CreateNewPost() {
   const dispatch = useDispatch();
   const createNewPostSelector = useSelector(
     (state: RootState) => state.overlayReducer
   );
+  const [file, setFile] = useState<File | null>(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+  const [imgages, setImages] = useState<imagesType[] | null>(null);
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const selectedFile = event.target.files[0];
+
+      if (
+        selectedFile.type.startsWith("image/") ||
+        selectedFile.type.startsWith("video/")
+      ) {
+        setFile(selectedFile);
+      } else {
+        // Handle unsupported file type
+        alert("Unsupported file type. Please select an image or video.");
+      }
+    }
+  };
 
   return (
     createNewPostSelector.isShowCreateNewPost && (
-      <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 z-[55] bg-red-500">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse sequi
-        asperiores laboriosam quas illo nam omnis accusantium officia accusamus
-        est quidem perferendis, eius dolore mollitia facere, sit tempora libero
-        beatae?
+      <div className="absolute select-none top-1/2 right-1/2 w-[496px] h-[calc(67%+27px)] translate-x-1/2 -translate-y-1/2 z-[55] bg-white rounded-xl overflow-hidden">
+        {/* Top */}
+        <div>
+          <h4 className="font-[600] text-center py-3 border-b border-[#dbdbdb]">
+            Create new post
+          </h4>
+        </div>
+
+        {/* Body */}
+        {/* First Step */}
+        {/* <div className="w-full h-[91%] flex flex-col gap-y-4 items-center justify-center p-6 ">
+          <div>
+            <svg className="w-24 h-[77px]">
+            <use href="#create-new-post"></use>
+          </svg>
+          <span className="text-xl">Drag photos and videos here</span>
+          <div>
+            <div className="flex flex-col items-center">
+              <button className="primary-btn" onClick={handleClick}>
+                Select from computer
+              </button>
+              {file && (
+                <div>
+                  {file.type.startsWith("image/") ? (
+                    <img src={URL.createObjectURL(file)} alt="Selected Image" />
+                  ) : (
+                    <video autoPlay>
+                      <source
+                        src={URL.createObjectURL(file)}
+                        type={file.type}
+                      />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                </div>
+              )}
+            </div>
+            <input
+              type="file"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+              accept="image/*,video/*"
+            />
+          </div>
+          </div>
+          </div> */}
+
+        {/* Second Step */}
+        <div className="relative w-full h-[91%]">
+          <div className="absolute inset-0">
+            <Swiper
+              navigation={true}
+              pagination={true}
+              thumbs={{ swiper: thumbsSwiper }}
+              modules={[FreeMode, Navigation, Pagination, Thumbs]}
+              className="create-new-post-mySwiper2 h-full"
+            >
+              <SwiperSlide>
+                <div>
+                  <img
+                    src="images/users/user9.jpg"
+                    alt=""
+                    className=" w-full h-full"
+                  />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <img
+                    src="images/users/user10.jpg"
+                    alt=""
+                    className=" w-full h-full"
+                  />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+          <div className="absolute bottom-4 right-6 left-6 flex justify-between items-center z-10">
+            <div className="flex items-center gap-x-6">
+              <div>
+                <button className="text-white hover:opacity-70 transition-all">
+                  <svg className="w-4 h-4">
+                    <use href="#aspect-ratio"></use>
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <button className="text-white hover:opacity-70 transition-all">
+                  <svg className="w-4 h-4">
+                    <use href="#zoom"></use>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="relative">
+              <button className="text-white hover:opacity-70 transition-all">
+                <svg className="w-4 h-4">
+                  <use href="#multiple-post"></use>
+                </svg>
+              </button>
+
+              <div className="absolute -top-32 right-0 h-[118px] bg-[#1a1a1a] opacity-80 p-2 rounded-lg">
+                <div className="flex gap-x-2">
+                  {/* Slides */}
+                  <Swiper
+                    onSwiper={(swiper) => setThumbsSwiper(swiper)}
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    freeMode={true}
+                    watchSlidesProgress={true}
+                    navigation={true}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="create-new-post-mySwiper w-96"
+                  >
+                    <SwiperSlide>
+                      <div className="relative w-[94px] h-[94px]">
+                        <button className="absolute top-2 right-2 text-white hover:opacity-70 ">
+                          <svg className="w-3 h-3">
+                            <use href="#close"></use>
+                          </svg>
+                        </button>
+                        <img src="images/users/user9.jpg" alt="" />
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="relative w-[94px] h-[94px]">
+                        <button className="absolute top-2 right-2 text-white hover:opacity-70 ">
+                          <svg className="w-3 h-3">
+                            <use href="#close"></use>
+                          </svg>
+                        </button>
+                        <img src="images/users/user10.jpg" alt="" />
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
+                  <div>
+                    <button className="w-12 h-12 flex items-center justify-center border rounded-full">
+                      <svg className="w-[22px] h-[22px] text-neutral-300">
+                        <use href="#plus"></use>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
--- src/Components/SvgSymbol.tsx
+++ src/Components/SvgSymbol.tsx
@@ -428,6 +428,81 @@ function SvgSymbol() {
         <title></title>
         <path d="M21 17.502a.997.997 0 0 1-.707-.293L12 8.913l-8.293 8.296a1 1 0 1 1-1.414-1.414l9-9.004a1.03 1.03 0 0 1 1.414 0l9 9.004A1 1 0 0 1 21 17.502Z"></path>
       </symbol>
+      <symbol
+        id="create-new-post"
+        aria-label="Icon to represent media such as images or videos"
+        className="x1lliihq x1n2onr6"
+        color="currentColor"
+        fill="currentColor"
+        role="img"
+        viewBox="0 0 97.6 77.3"
+      >
+        <title>Icon to represent media such as images or videos</title>
+        <path
+          d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M84.7 18.4 58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M78.2 41.6 61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z"
+          fill="currentColor"
+        ></path>
+      </symbol>
+      <symbol
+        id="multiple-post"
+        aria-label="Open media gallery"
+        className="_ab6-"
+        color="currentColor"
+        fill="currentColor"
+        role="img"
+        viewBox="0 0 24 24"
+      >
+        <path
+          d="M19 15V5a4.004 4.004 0 0 0-4-4H5a4.004 4.004 0 0 0-4 4v10a4.004 4.004 0 0 0 4 4h10a4.004 4.004 0 0 0 4-4ZM3 15V5a2.002 2.002 0 0 1 2-2h10a2.002 2.002 0 0 1 2 2v10a2.002 2.002 0 0 1-2 2H5a2.002 2.002 0 0 1-2-2Zm18.862-8.773A.501.501 0 0 0 21 6.57v8.431a6 6 0 0 1-6 6H6.58a.504.504 0 0 0-.35.863A3.944 3.944 0 0 0 9 23h6a8 8 0 0 0 8-8V9a3.95 3.95 0 0 0-1.138-2.773Z"
+          fill-rule="evenodd"
+        ></path>
+      </symbol>
+      <symbol
+        id="zoom"
+        aria-label="Select zoom"
+        className="_ab6-"
+        color="currentColor"
+        fill="currentColor"
+        height="16"
+        role="img"
+        viewBox="0 0 24 24"
+        width="16"
+      >
+        <path d="m22.707 21.293-4.825-4.825a9.519 9.519 0 1 0-1.414 1.414l4.825 4.825a1 1 0 0 0 1.414-1.414ZM10.5 18.001a7.5 7.5 0 1 1 7.5-7.5 7.509 7.509 0 0 1-7.5 7.5Zm3.5-8.5h-2.5v-2.5a1 1 0 1 0-2 0v2.5H7a1 1 0 1 0 0 2h2.5v2.5a1 1 0 0 0 2 0v-2.5H14a1 1 0 0 0 0-2Z"></path>
+      </symbol>
+      <symbol
+        id="aspect-ratio"
+        aria-label="Select crop"
+        className="_ab6-"
+        color="currentColor"
+        fill="currentColor"
+        height="16"
+        role="img"
+        viewBox="0 0 24 24"
+        width="16"
+      >
+        <path d="M10 20H4v-6a1 1 0 0 0-2 0v7a1 1 0 0 0 1 1h7a1 1 0 0 0 0-2ZM20.999 2H14a1 1 0 0 0 0 2h5.999v6a1 1 0 0 0 2 0V3a1 1 0 0 0-1-1Z"></path>
+      </symbol>
+      <symbol
+        id="plus"
+        aria-label="Plus icon"
+        className="x1lliihq x1n2onr6"
+        color="currentColor"
+        fill="currentColor"
+        role="img"
+        viewBox="0 0 24 24"
+      >
+        <title>Plus icon</title>
+        <path d="M21 11.3h-8.2V3c0-.4-.3-.8-.8-.8s-.8.4-.8.8v8.2H3c-.4 0-.8.3-.8.8s.3.8.8.8h8.2V21c0 .4.3.8.8.8s.8-.3.8-.8v-8.2H21c.4 0 .8-.3.8-.8s-.4-.7-.8-.7z"></path>
+      </symbol>
     </svg>
   );
 }
