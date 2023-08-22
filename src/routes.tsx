17f8e99ad7e0413ba108a1a1b09c0872198377d2
masoud <masoud.red64@gamil.com>
2023-08-22 12:17:48 +0330

    create new page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Index from "./pages/Index";
 import Notifications from "./pages/Notifications";
 import Stories from "./pages/Stories";
+import Explore from "./pages/explore";
 
 const routes = [
   {
@@ -10,6 +11,7 @@ const routes = [
     children: [
       { path: "", element: <Home /> },
       { path: "notifications", element: <Notifications /> },
+      { path: "explore", element: <Explore /> },
     ],
   },
   {
