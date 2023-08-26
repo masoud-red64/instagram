7dbb046e988eedf74db1b2c6ae735f9fc68c10f0
masoud <masoud.red64@gamil.com>
2023-08-26 17:04:05 +0330

    MyProfile page => responsive top

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -15,23 +15,29 @@ function MyProfile() {
     { id: 4, img: "highlight4.jpg" },
     { id: 5, img: "highlight5.jpg" },
     { id: 6, img: "highlight6.jpg" },
+    { id: 6, img: "highlight6.jpg" },
+    { id: 6, img: "highlight6.jpg" },
+    { id: 6, img: "highlight6.jpg" },
+    { id: 6, img: "highlight6.jpg" },
   ];
 
   const { username } = useParams();
   if (username === "masoud_red64") {
     return (
-      <div className="lg:container lg:mx-auto sm:pt-[30px] sm:px-5">
-        <header className="flex items-center gap-x-[30px] mb-11">
-          <div className="grow">
-            <div className="max-w-[150px] max-h-[150px] mx-auto">
+      <div className="md:container md:mx-auto sm:pt-[30px] sm:px-5">
+        <header className="flex items-center gap-x-[30px] mt-4 mx-4 mb-6 md:m-0 md:mb-11">
+          <div className="grow shrink-0">
+            <div className="w-[77px] h-[77px] md:w-[150px] md:h-[150px] mx-auto">
               <Story id={1} img="user1.jpg" hasStory hasNewStory />
             </div>
           </div>
           <div className="grow-[2] flex flex-col gap-y-5">
             <div className="flex items-center">
               <span className="text-xl mr-5">masoud_red64</span>
-              <button className="secondary-btn mr-2">Edit profile</button>
-              <button className="secondary-btn mr-3">View Archive</button>
+              <div className="hidden md:flex items-center gap-x-2">
+                <button className="secondary-btn">Edit profile</button>
+                <button className="secondary-btn mr-3">View Archive</button>
+              </div>
               <button>
                 <svg className="w-6 h-6">
                   <use href="#setting"></use>
@@ -39,7 +45,12 @@ function MyProfile() {
               </button>
             </div>
 
-            <div className="flex items-center gap-x-10">
+            <div className="flex md:hidden items-center gap-x-2">
+              <button className="secondary-btn">Edit profile</button>
+              <button className="secondary-btn mr-3">View Archive</button>
+            </div>
+
+            <div className="hidden md:flex items-center gap-x-10">
               <div className="flex gap-x-1">
                 <span className="font-[600]">0</span>
                 <span>posts</span>
@@ -54,7 +65,7 @@ function MyProfile() {
               </div>
             </div>
 
-            <div className="flex flex-col">
+            <div className="hidden md:flex flex-col">
               <span className="text-sm font-[600]">M҉a҉s҉o҉u҉d҉</span>
               <span className="leading-[18px]">
                 ▷ ●━━━━━━─────── ♪🖤
@@ -64,19 +75,36 @@ function MyProfile() {
             </div>
           </div>
         </header>
+        <div className="flex md:hidden flex-col px-4 pb-5">
+          <span className="text-sm font-[600]">M҉a҉s҉o҉u҉d҉</span>
+          <span className="leading-[18px]">
+            ▷ ●━━━━━━─────── ♪🖤
+            <br />
+            ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤㅤㅤ↻
+          </span>
+        </div>
 
         <main>
           <section>
             <Swiper
-              slidesPerView={7}
+              slidesPerView={3}
+              spaceBetween={10}
+              breakpoints={{
+                640: {
+                  slidesPerView: 6,
+                },
+                1024: {
+                  slidesPerView: 8,
+                },
+              }}
               navigation={true}
               modules={[Navigation]}
-              className="flex"
+              className="w-64 container xs:w-full"
             >
               {userHighlights.map((highlight) => (
                 <SwiperSlide>
                   <div className="flex flex-col items-center gap-y-3">
-                    <div className="w-[77px] h-[77px]">
+                    <div className="w-14 h-14 md:w-[77px] md:h-[77px]">
                       <Story
                         img={`/highlights/${highlight.img}`}
                         hasStory
@@ -89,8 +117,8 @@ function MyProfile() {
               ))}
               <SwiperSlide>
                 <div className="flex flex-col items-center gap-y-3">
-                  <div className="w-[77px] h-[77px] flex items-center justify-center bg-zinc-50 border rounded-full">
-                    <svg className="w-11 h-11 text-[#c7c7c7]">
+                  <div className="w-14 h-14 md:w-[77px] md:h-[77px] flex items-center justify-center bg-zinc-50 border rounded-full">
+                    <svg className="w-[30px] h-[30px] md:w-11 md:h-11 text-[#c7c7c7]">
                       <use href="#plus"></use>
                     </svg>
                   </div>
